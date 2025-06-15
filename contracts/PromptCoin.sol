f306a7469926a4abd42cbf80414dc36b1ac41511
caohongyu <caooooohy@gmail.com>
2025-06-16 00:18:13 +0800

    Update PromptCoin.sol

--- contracts/PromptCoin.sol
+++ contracts/PromptCoin.sol
@@ -2,12 +2,11 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // PromptCoin Contract
 contract PromptCoin is ERC20, Ownable {
-    constructor() ERC20("PromptCoin", "PTC") Ownable(msg.sender){
+    constructor() ERC20("PromptCoin", "PTC"){
         _mint(msg.sender, 33_000_000 * 1e18);
     }
 }
